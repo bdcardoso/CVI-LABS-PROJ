e49a1f8e9ddae6253634d482bea3695c890247e6
Bruno Cardoso <brunocardoso.lpma@gmail.com>
2018-04-18 18:37:30 +0100

    new file name

--- PROJ1/proj1.m
+++ PROJ1/proj1.m
@@ -32,21 +32,14 @@ It = im2bw(I,T1);
 It2 = imfill(binaryImageR,'holes');
 figure, imshow(It2);
 
-%It = im2bw(I,T1);
-%figure, imshow(It);
+
 
 [lb num] = bwlabel(It2); %estava BW3
-%figure,
-%subplot(1,3,1); imshow(mat2gray(lb)); title('Etiquetas');
-%subplot(1,3,1); imshow(label2rgb(lb));title('Labels')
+
 stats = regionprops(lb);
 areas = [stats.Area];
 [dummy indM] = max(areas);
 imgBr = (lb == indM);
-%subplot(1,3,2);imshow(imgBr);title('Maior área');
-%subplot(1,3,3);imshow(I);title('Cérebro');
-%alternativa
-%subplot(1,3,4);imshow(double(imgg).*(imgBr));title('Cérebro');
 n_ojb=0
 areas = [];
 figure; imshow(lb); hold on;
@@ -64,8 +57,8 @@ end
 
     
 imgBr1 = (lb == ind);
-%plot(1,1);imshow(imgBr1);title('Maior área');
-%plot(1,2);imshow(I);title('Cérebro');
+%plot(1,1);imshow(imgBr1);title('Maior ï¿½rea');
+%plot(1,2);imshow(I);title('Cï¿½rebro');
 
 
 %plot(stats(ind).Centroid(1),stats(ind).Centroid(2), '.r','markersize',50);
