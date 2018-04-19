a59ccee0dcf16168a54693f01528f4774dd18731
nadia-mourao <nadialm_16@hotmail.com>
2018-04-19 16:47:17 +0100

    trys

--- BRUNO-1/nadia2.m
+++ BRUNO-1/nadia2.m
@@ -2,18 +2,24 @@ close all;
 clear all; 
 clc;
 
-I1 = imread('Moedas4.jpg');
+color = char('r','g','b','y','m','c','w');
+
+I1 = imread('Moedas2.jpg');
 figure, imshow(I1);
+ 
+ %I=rgb2hsv(I1);
+ %figure, imshow(I);
 
-I=rgb2gray(I1);
+ %[h,w]=size(I);
+ %figure;imshow(I);
+ %I=rgb2gray(I1);
+% A = im2bw(I1, 0.6);
+ %c = edge(A,'log');
+ %I=rgb2gray(I);
+ %c = edge(I, 'canny',0.2);  
+ %figure, imshow(c);
 
 
-[h,w]=size(I);
-%figure;imshow(I);
-
-c = edge(I, 'canny',0.4);  
-
-I1 = cat(1,I1,c);
 figure, imshow(I1);
 %I1 = imsharpen(I1,'Radius', 2, 'Amount',1);
 %figure, imshow(I1);
@@ -60,22 +66,14 @@ imwrite( mapped_image, 'YourData.tif' )
 
 I2 = imread('YourData.tif');
 %I2 = rgb2gray(I2);
-T1 = graythresh(I2);
-c = edge(I2, 'canny',0.01);  
 
-%d2 = imfill(c, 'holes');  
+d2 = imfill(I2, 'holes');  
+%figure, imshow(d2);        
 
-SE = strel('square',3);
-SE = [0 1 0
-      1 1 1
-      0 1 0];
-BW = imdilate(c,SE);
+Label=bwlabel(d2,4);
 
-%BW2 =imerode(BW,SE);
 
-d2 = imfill(BW, 'holes');  
-
-[lb num] = bwlabel(c); %estava BW3
+[lb num] = bwlabel(d2); %estava BW3
 
 stats = regionprops(lb);
 areas = [stats.Area];
@@ -85,6 +83,15 @@ n_ojb=0
 areas = [];
 figure; imshow(lb); hold on;
 
+% for k=1: num
+%     areas = [areas length(find(lb==k))]
+%     if all(areas(k)>500)
+%         plot(stats(k).Centroid(1),stats(k).Centroid(2), '.r','markersize',25);
+%         drawnow;
+%         
+%     end
+% 
+% end
 
 
 %figure; imshow(lb);title(''); hold on;
@@ -92,17 +99,11 @@ figure; imshow(lb); hold on;
 
 %TESTING TESTING TESTING - LIGAR OBJECTOS 
 
-ma = cellstr(num2str(classlabel(:)));    %num2str can process vector
-
 boundaries = bwboundaries(d2);
 numberOfBoundaries = size(boundaries, 1);
 for k = 1 : numberOfBoundaries
 	thisBoundary = boundaries{k};
-	plot(thisBoundary(:,2), thisBoundary(:,1), 'r', 'LineWidth', 3);
-    plot(stats(k).Centroid(1),stats(k).Centroid(2), '.r','markersize',25);
-    
-    plot(x(i),y(i),'Marker',ma{k},'MarkerSize',fontsize,'Color',colors(7));
-    
+	plot(thisBoundary(:,2), thisBoundary(:,1), color(randi(numel(color))), 'LineWidth', 3);
 end
 hold off;
 
@@ -149,20 +150,95 @@ for b1 = 1 : numberOfBoundaries
 		% Find the overall min distance
 		minDistance = min(minDistance);
 		% Report to command window.
-        
 		fprintf('The minimum distance from region %d to region %d is %.3f pixels\n', b1, b2, minDistance);
 
 		% Draw a line between point 1 and 2
-        color = char('r','g','b','y','m','c','w','k');
+        
 
 
         
         
-		line([x1, x2], [y1, y2], 'Color', 'y', 'LineWidth', 1);
+		line([x1, x2], [y1, y2], 'Color', color(randi(numel(color))), 'LineWidth', 1);
 	end
 end
 
 %annotation('Found %d regions\n', b1,[2 3 300 200])
 fprintf('Found %d regions\n', b1);
 
- 
+%DETECCAO DE MOEDAS DAQUI PARA BAIXO
+
+a1=(Label==1);
+a2=(Label==2);
+a3=(Label==3);
+a4=(Label==4);
+a5=(Label==5);
+a6=(Label==6);
+a7=(Label==7);
+a8=(Label==8);
+a9=(Label==9);
+a10=(Label==10);
+
+
+D1 = bwdist(~a1);           
+%figure, imshow(D1,[]),        
+[xc1 yc1 r1]=distance(D1);
+f1=whichcoin(r1)
+disp(r1)
+
+
+D2 = bwdist(~a2);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D2,[]),      %  
+[xc2 yc2 r2]=distance(D2);
+f2=whichcoin(r2)
+disp(r2)
+
+D3 = bwdist(~a3);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D3,[]),      %  
+[xc3 yc3 r3]=distance(D3);
+f3=whichcoin(r3)
+disp(r3)
+
+D4 = bwdist(~a4);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D4,[]),      %  
+[xc4 yc4 r4]=distance(D4);
+f4=whichcoin(r4)
+disp(r4)
+
+D5 = bwdist(~a5);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D5,[]),      %  
+[xc5 yc5 r5]=distance(D5);
+f5=whichcoin(r5)
+disp(r5)
+
+D6 = bwdist(~a6);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D6,[]),      %  
+[xc6 yc6 r6]=distance(D6);
+f6=whichcoin(r6)
+disp(r6)
+
+D7 = bwdist(~a7);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D7,[]),      %  
+[xc7 yc7 r7]=distance(D7);
+f7=whichcoin(r7)
+disp(r7)
+
+D8 = bwdist(~a8);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D8,[]),      %  
+[xc8 yc8 r8]=distance(D8);
+f8=whichcoin(r8)
+disp(r8)
+
+D9 = bwdist(~a9);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D9,[]),      %  
+[xc9 yc9 r9]=distance(D9);
+f9=whichcoin(r9)
+disp(r9)
+
+D10 = bwdist(~a10);           % computing minimal euclidean distance to non-white pixel 
+%figure, imshow(D10,[]),      %  
+[xc10 yc10 r10]=distance(D10);
+f10=whichcoin(r10)
+disp(r10)
+
+
+
